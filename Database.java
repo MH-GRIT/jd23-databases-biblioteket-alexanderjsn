1b2d5b392e1d0ea80473f3d892fd0eb44af276d7
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-01-16 11:22:42 +0100

    satte upp databas klass m. singleton

--- Database.java
+++ Database.java
@@ -1,2 +1,41 @@
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Database {
+    private static String username = "root";
+    private static String password = "103eec29f";
+    private static String url = "localhost";
+    private static int port = 3306;
+    private static String databaseName = "LibraryDatabase";
+    private static Database instance;
+    private MysqlDataSource dataSource;
+
+    private Database() {
+    initializeDataSource();
+    }
+
+    ;
+
+
+    private void initializeDataSource() {
+    dataSource = new MysqlDataSource();
+    dataSource.setUser(username);
+    dataSource.setPassword(password);
+    dataSource.setURL("jdbc:mysql://" + url + ":" + port + "/" + databaseName + "?serverTimezone=UTC");
+    }
+
+    private static Database getInstance(){
+        if (instance == null){
+            instance = new Database();
+        }
+
+        return instance;
+    }
+
+
+    public Connection getConnection() throws SQLException{
+        return dataSource.getConnection();
+    }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public void test(){
-        int test=1;
 
 
     }
