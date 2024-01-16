2e2b70da81b74936c0fe45d6cb415d8b75c893eb
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-01-16 12:03:20 +0100

    satte upp connection

--- Database.java
+++ Database.java
@@ -8,34 +8,32 @@ public class Database {
     private static String password = "103eec29f";
     private static String url = "localhost";
     private static int port = 3306;
-    private static String databaseName = "LibraryDatabase";
+    private static String databaseName = "Library";
     private static Database instance;
     private MysqlDataSource dataSource;
 
     private Database() {
-    initializeDataSource();
+        initializeDataSource();
     }
+        ;
 
-    ;
 
-
-    private void initializeDataSource() {
-    dataSource = new MysqlDataSource();
-    dataSource.setUser(username);
-    dataSource.setPassword(password);
-    dataSource.setURL("jdbc:mysql://" + url + ":" + port + "/" + databaseName + "?serverTimezone=UTC");
-    }
-
-    private static Database getInstance(){
-        if (instance == null){
-            instance = new Database();
+        private void initializeDataSource () {
+            dataSource = new MysqlDataSource();
+            dataSource.setUser(username);
+            dataSource.setPassword(password);
+            dataSource.setURL("jdbc:mysql://" + url + ":" + port + "/" + databaseName + "?serverTimezone=UTC");
         }
 
-        return instance;
-    }
+        public static Database getInstance() {
+            if (instance == null) {      // om ingen instans av databasen är skapad
+                instance = new Database();  // skapa en instans av databas
+            }
+            return instance;
+        }
 
 
-    public Connection getConnection() throws SQLException{
-        return dataSource.getConnection();
+        public Connection getConnection () throws SQLException {
+            return dataSource.getConnection();
+        }
     }
-}
--- Main.java
+++ Main.java
@@ -1,6 +1,32 @@
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
-    public void test(){
+    public static void main(String[] args) {
 
+    try{
+        Connection conn = (Connection) Database.getInstance();
+        if (conn != null){
+        System.out.println("Connection worked");
 
+        String sql = "SELECT * FROM bookTable";
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+
+        while(rs.next()){
+            System.out.println(rs.getInt("bookID") + rs.getString("bookName") + rs.getInt("stock"));
+        }
+        stmt.close();
+        conn.close();
+        rs.close();
+    }
+    }
+    catch (Exception e) {
+        System.exit(1);
+        throw new RuntimeException(e);
+    }
     }
 }
