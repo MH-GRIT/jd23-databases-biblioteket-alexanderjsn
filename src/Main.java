4e5036d145d942c102d3c97d894d0cc9f0c46097
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-01-16 14:17:41 +0100

    fixade så man kan logga in i konsol och registrera ny användare

--- src/Main.java
+++ src/Main.java
@@ -7,75 +7,99 @@ import java.util.Scanner;  // Import the Scanner class
 public class Main {
     public static void main(String[] args) {
 
-
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter new name: ");
-        String userScanner = scanner.nextLine();
-
-        System.out.println("Enter new email: ");
-        String emailScanner = scanner.nextLine();
-
-        System.out.println("Enter new phone: ");
-        String phoneScanner = scanner.nextLine();
-
-        System.out.println("Enter new password: ");
-        String passwordScanner = scanner.nextLine();
-
-        System.out.println("Enter new Username: ");
-        String userNameScanner = scanner.nextLine();
-
-
-        String nameInput = userScanner;
-        String emailInput = emailScanner;
-        String phoneInput = phoneScanner;
-        String passwordInput = passwordScanner;
-        String usernameInput = userNameScanner;
-
-
         try {
+
             // skapar connection
             Connection conn = Database.getInstance().getConnection();
+            Scanner scanner;
             if (conn != null) {
                 System.out.println("Connected to the database!");
 
+                scanner = new Scanner(System.in);
+
+                System.out.println("Choose option: 1. register  2. log in: ");
+                int choice = Integer.parseInt(scanner.nextLine());
+                if (choice == 1) {
+
+                    System.out.println("Enter new name: ");
+                    String userScanner = scanner.nextLine();
+
+                    System.out.println("Enter new email: ");
+                    String emailScanner = scanner.nextLine();
+
+                    System.out.println("Enter new phone: ");
+                    String phoneScanner = scanner.nextLine();
+
+                    System.out.println("Enter new password: ");
+                    String passwordScanner = scanner.nextLine();
+
+                    System.out.println("Enter new Username: ");
+                    String userNameScanner = scanner.nextLine();
 
 
+                    String nameInput = userScanner;
+                    String emailInput = emailScanner;
+                    String phoneInput = phoneScanner;
+                    String passwordInput = passwordScanner;
+                    String usernameInput = userNameScanner;
+                    String newUser = "INSERT INTO userTable (name, email, phone, password, username) VALUES (?,?,?,?,?)";
+                    PreparedStatement pstmt = conn.prepareStatement(newUser);
+                    pstmt.setString(1, nameInput);
+                    pstmt.setString(2, emailInput);
+                    pstmt.setString(3, phoneInput);
+                    pstmt.setString(4, passwordInput);
+                    pstmt.setString(5, usernameInput);
 
-                String newUser = "INSERT INTO userTable (name, email, phone, password, username) VALUES (?,?,?,?,?)";
-                PreparedStatement pstmt = conn.prepareStatement(newUser);
-                pstmt.setString(1, nameInput);
-                pstmt.setString(2, emailInput);
-                pstmt.setString(3, phoneInput);
-                pstmt.setString(4, passwordInput);
-                pstmt.setString(5, usernameInput);
+                    // Visar antalet påverkade/uppdaterade rader
+                    int affectedRows = pstmt.executeUpdate();
+                    System.out.println("Rows affected: " + affectedRows);
+                    pstmt.close();
 
-                // Visar antalet påverkade/uppdaterade rader
-                int affectedRows = pstmt.executeUpdate();
-                System.out.println("Rows affected: " + affectedRows);
+                } else if (choice == 2) {
 
+                    System.out.println("Enter new username: ");
+                    String insertUsername = scanner.nextLine();
+                    System.out.println("Enter new password: ");
+                    String insertPassword = scanner.nextLine();
 
-                String getBooks = "SELECT * FROM userTable";
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(getBooks);
+                    String checkLogin = "SELECT password FROM userTable WHERE username = ?";
+                    try (PreparedStatement loginPstmt = conn.prepareStatement(checkLogin)) {
+                        loginPstmt.setString(1, insertUsername);
+                        ResultSet loginRs = loginPstmt.executeQuery();
+                        if (loginRs.next()) {
+                            String existingPassword = loginRs.getString("password");
+                            if (insertPassword.equals(existingPassword)) {
+                                System.out.println("Log in succesful!");
+                            } else {
+                                System.out.println("Login not successful");
+                            }
+                        }
+                    }
 
-                while(rs.next()){
-                    System.out.println( rs.getString("name ") + rs.getString("email ") + rs.getString("phone ") + rs.getString("password ") + rs.getString("username "));
                 }
+            }
+        } catch (SQLException e) {
+            System.exit(1);
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
+
+            /*String getBooks = "SELECT * FROM userTable";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(getBooks);
+
+            while(rs.next()){
+                System.out.println( rs.getString("name") + rs.getString("email") + rs.getString("phone") + rs.getString("password") + rs.getString("username"));
+            }
 
 
                 /*while(rs.next()){
                     System.out.println(rs.getInt("bookID") + rs.getString("bookName") + rs.getInt("stock"));
-                }*/
-                rs.close();
-                stmt.close();
-                pstmt.close();
-                conn.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
-}
+                }
+            rs.close();
+            stmt.close();
+            conn.close();*/
