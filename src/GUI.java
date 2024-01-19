57a920082cbb293c930febf7969369a7b90eb0e3
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-01-19 05:58:57 +0100

    optimerade kod. plus optimerade SQL med "join"

--- src/GUI.java
+++ src/GUI.java
@@ -1,85 +1,74 @@
 package src;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.sql.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Calendar;
+
 
 public class GUI {
 
-    public String bookName;
+    private String bookName;
 
-    public String reservedBookSelected;
+    private String reservedBookSelected;
 
-    public JTextField usernameTextfield;
-    public JPasswordField passwordTextfield;
-    public String passwordInput;
-    public boolean userLogin = false;
-    public JPanel homepagePanel = new JPanel();
-    public JFrame frame = new JFrame("Library");
+    private JTextField usernameTextfield;
+    private JPasswordField passwordTextfield;
+    private String passwordInput;
+    private final JPanel homepagePanel = new JPanel();
+    private final JFrame frame = new JFrame("Library");
 
-    public JPanel mainPanel = new JPanel();
-    public JPanel editInfoPanel = new JPanel();
+    private final JPanel mainPanel = new JPanel();
+    private final JPanel editInfoPanel = new JPanel();
 
-    CardLayout cl = new CardLayout();
-
-
-    //register variables:
-    JTextField nametextField = new JTextField("Namn: ");
-    JTextField emailTextfield = new JTextField("Mail: ");
-    JTextField phoneTextfield = new JTextField("Telefon: ");
-    JTextField newusernameTextfield = new JTextField("Användarnamn: ");
-    JTextField newpasswordTextfield = new JTextField("Lösenord: ");
+    private final CardLayout cl = new CardLayout();
 
 
-    //homepage
-
-    DefaultTableModel bookDTable = new DefaultTableModel();
-    DefaultTableModel reservedbookDTable = new DefaultTableModel();
-
-    JTable bookTable = new JTable(bookDTable);
-    JTable reservedbookTable = new JTable(reservedbookDTable);
+    private JTextField nametextField = new JTextField("Namn: ");
+    private JTextField emailTextfield = new JTextField("Mail: ");
+    private  JTextField phoneTextfield = new JTextField("Telefon: ");
+    private  JTextField newusernameTextfield = new JTextField("Användarnamn: ");
+    private   JTextField newpasswordTextfield = new JTextField("Lösenord: ");
 
 
-    JScrollPane scrollPane = new JScrollPane(bookTable);
-    JScrollPane reservedscrollPane = new JScrollPane(reservedbookTable);
+
+    private final DefaultTableModel bookDTable = new DefaultTableModel();
+    private final DefaultTableModel reservedbookDTable = new DefaultTableModel();
+
+    private final JTable bookTable = new JTable(bookDTable);
+    private final JTable reservedbookTable = new JTable(reservedbookDTable);
+
+
+    private final JScrollPane scrollPane = new JScrollPane(bookTable);
+    private final JScrollPane reservedscrollPane = new JScrollPane(reservedbookTable);
 
 
     // edit info page
-    DefaultTableModel editDTable = new DefaultTableModel();
-    JTable editTable = new JTable(editDTable);
-    JScrollPane editscrollPane = new JScrollPane(editTable);
+    private final DefaultTableModel editDTable = new DefaultTableModel();
 
-    JPanel userinfoPanel = new JPanel();
+    private final JPanel userinfoPanel = new JPanel();
 
 
-    JTextField searchField;
+    private JTextField searchField;
 
 
     // update:
-    JTextField insertName = new JTextField();
-    JTextField nameLabel = new JTextField();
-    JTextField emailLabel = new JTextField();
-    JTextField phoneLabel = new JTextField();
-    JTextField passwordLabel = new JTextField();
-    JTextField usernameLabel = new JTextField();
-    public JPanel registerPanel = new JPanel();
+    private  JTextField nameLabel = new JTextField();
+    private  JTextField emailLabel = new JTextField();
+    private  JTextField phoneLabel = new JTextField();
+    private  JTextField passwordLabel = new JTextField();
+    private  JTextField usernameLabel = new JTextField();
+    private  JPanel registerPanel = new JPanel();
 
 
     //history
-    public JPanel historyPanel = new JPanel();
+    private final JPanel historyPanel = new JPanel();
 
-    public JPanel loginPanel = new JPanel();
+    private JPanel loginPanel = new JPanel();
 
 
-    public GUI() throws SQLException {
+    public GUI()  {
 
         frame.add(mainPanel);
         mainPanel.setLayout(cl);
@@ -157,21 +146,7 @@ public class GUI {
 
     }
 
-    /*public void alreadyExist() throws SQLException {
-        try (Connection conn = Database.getInstance().getConnection()) {
-            String checkUser = "SELECT name, email FROM userTable WHERE name = ? AND email = ?";
-            PreparedStatement checkUserPSTMT = conn.prepareStatement(checkUser);
-            checkUserPSTMT.setString(1, nametextField.getText());
-            checkUserPSTMT.setString(2, emailTextfield.getText());
-            ResultSet ifExistRS = checkUserPSTMT.executeQuery();
-            if(ifExistRS.next()){
-                System.out.println("User exists");
-            }
-            else{
-                System.out.println("User no exist");
-            }
-        }
-}*/
+
     public void checkBooks() {
         String searchBook = "SELECT * FROM bookTable WHERE bookName LIKE ? OR author LIKE ? ORDER BY bookName";
         String searchInput = searchField.getText();
@@ -197,17 +172,7 @@ public class GUI {
             throw new RuntimeException(e);
         }
     }
-        /*public void updateInfo() throws SQLException {
 
-                    String updateSQL = "UPDATE userTable SET name = ? WHERE username = ?";
-                    try (PreparedStatement updatePstmt = conn.prepareStatement(updateSQL)){
-                        updatePstmt.setString(1, nameUpdate);
-                        updatePstmt.setString(2, insertUsername);
-                        int rowsUpdated = updatePstmt.executeUpdate();
-                        System.out.println("Update done!");
-                    }
-                }
-        }*/
 
     public void retrieveInfo() {
         try (Connection conn = Database.getInstance().getConnection()) {
@@ -258,15 +223,6 @@ public class GUI {
             throw new RuntimeException(e);
         }
     }
-
-
-                   /* while (userInfoRS.next()) {
-                        existingUser = userInfoRS.getString("name");
-                        userArray.add(existingUser);
-                    }
-                    for (String user : userArray) {
-                        editDTable.addRow(new Object[]{user});
-                    }*/
 
 
 
@@ -342,8 +298,8 @@ public class GUI {
 
                     System.out.println(bookID + " booked!");
 
-                    int updateReservation = binfoPSTMT.executeUpdate();
-                    int reserveBook = reservedPSTMT.executeUpdate();
+                    binfoPSTMT.executeUpdate();
+                    reservedPSTMT.executeUpdate();
 
                 } else {
                     System.out.println("Book is currently not available. Return date is: " + returnDate);
@@ -352,9 +308,8 @@ public class GUI {
         }
     }
 
-    public void history() {  // optimerad
+    public void history() {
 
-        // join
         String reservedInfo = "SELECT reserveBook.returnDate, reserveBook.borrowedDate, reserveBook.bookName " +
                                 "FROM reserveBook " +
                                 "JOIN userTable ON reserveBook.userID = userTable.userID " +
@@ -387,26 +342,25 @@ public class GUI {
     public void returnBook() throws SQLException {
         try (Connection conn = Database.getInstance().getConnection()) {
             int bookID = 0, userID = 0;
-            // hämtar bokid efter boknamnet ( som hämtas från den row användaren trycker på i tabellen )
-            String bookInfo = "SELECT bookID FROM bookTable WHERE bookName = ?";
+                                // hämta bookID och userID inuti reservebook (foreign key)
+            String bookInfo = "SELECT reserveBook.bookID, reserveBook.userID FROM reserveBook reserveBook " +
+                                // sätt ihop usertable och reservebook (samma med bok )
+                              "JOIN userTable userTable ON reserveBook.userID = userTable.userID " +
+                            " JOIN bookTable bookTable ON reserveBook.bookID = bookTable.bookID " +
+                            // specifikerar vilken del av reserveTable
+                            "WHERE userTable.username = ? AND bookTable.bookName = ?";
+
             try (PreparedStatement retPSTMT = conn.prepareStatement(bookInfo)) {
-                retPSTMT.setString(1, reservedBookSelected);
+                retPSTMT.setString(1, usernameTextfield.getText());
+                retPSTMT.setString(2, reservedBookSelected);
+
                 try (ResultSet bookInfoRS = retPSTMT.executeQuery()) {
                     if (bookInfoRS.next()) {
-                        bookID = bookInfoRS.getInt("bookID");
+                        bookID = bookInfoRS.getInt("reserveBook.bookID");
+                        userID = bookInfoRS.getInt("reserveBook.userID");
                     }
                 }
             }
-                // samma princip
-                String reservedUserInfo = "SELECT userID FROM userTable WHERE username = ?";
-                try (PreparedStatement resusPSTMT = conn.prepareStatement(reservedUserInfo)) {
-                    resusPSTMT.setString(1, usernameTextfield.getText());
-                    try (ResultSet userInfoRS = resusPSTMT.executeQuery()) {
-                        if (userInfoRS.next()) {
-                            userID = userInfoRS.getInt("userID");
-                        }
-                    }
-                }
                     // sätter available på sann (bok lämnas tillbaka) på bok ID som valts
                     String returnBook = "UPDATE bookTable SET available = ? WHERE bookID = ?";
                     try (PreparedStatement returnedPSTMT = conn.prepareStatement(returnBook)) {
@@ -419,7 +373,7 @@ public class GUI {
                         // tar bort ur reserveBook så användaren inte har den lånad  + rensar från historik menyn
                         String deleteResRow = "DELETE FROM reserveBook WHERE userID = ? AND bookID = ?";
                         try (PreparedStatement deletePstmt = conn.prepareStatement(deleteResRow)) {
-                            ;
+
                             deletePstmt.setInt(1, userID);
                             deletePstmt.setInt(2, bookID);
                             deletePstmt.executeUpdate();
@@ -492,12 +446,9 @@ public class GUI {
                         throw new RuntimeException(ex);
                     }
                 });
-                editInfoBTN.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        cl.show(mainPanel, "editInfoPanel");
-                        retrieveInfo();
-                    }
+                editInfoBTN.addActionListener(e -> {
+                    cl.show(mainPanel, "editInfoPanel");
+                    retrieveInfo();
                 });
 
 
@@ -526,8 +477,8 @@ public class GUI {
                 loginPanel = new JPanel();
                 loginPanel.setLayout(new GridLayout(5, 1));
 
-                JLabel usernameLabel = new JLabel("Användarnamn: ");
-                loginPanel.add(usernameLabel);
+                JLabel userLabel = new JLabel("Användarnamn: ");
+                loginPanel.add(userLabel);
                 usernameTextfield = new JTextField();
                 loginPanel.add(usernameTextfield);
 
@@ -540,23 +491,20 @@ public class GUI {
                 loginPanel.add(registerButton);
                 loginPanel.add(loginButton);
                 registerButton.addActionListener(e -> cl.show(mainPanel, "registerPanel"));
-                loginButton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        String usernameText = usernameTextfield.getText();
-                        passwordInput = passwordTextfield.getText();
-                        try {
-                            loginMethod();
-                        } catch (SQLException ex) {
-                            throw new RuntimeException(ex);
-                        }
+                loginButton.addActionListener(e -> {
+                    String usernameText = usernameTextfield.getText();
+                    passwordInput = passwordTextfield.getText();
+                    try {
+                        loginMethod();
+                    } catch (SQLException ex) {
+                        throw new RuntimeException(ex);
                     }
                 });
 
 
             }
             public void createRegisterPanel(){
-                registerPanel.setLayout(new GridLayout(6, 1));
+                registerPanel.setLayout(new GridLayout(6, 2));
                 JLabel nameLabel = new JLabel("Name:");
                 nametextField = new JTextField();
                 JLabel emailLabel = new JLabel("Email:");
@@ -566,6 +514,7 @@ public class GUI {
                 JLabel usernameLable = new JLabel("Username:");
                 newusernameTextfield = new JTextField();
                 JLabel passwordLabel = new JLabel("Password:");
+                JPanel innerRegisterPanel = new JPanel();
 
                 newpasswordTextfield = new JTextField();
 
@@ -580,7 +529,7 @@ public class GUI {
                 registerPanel.add(passwordLabel);
                 registerPanel.add(newpasswordTextfield);
                 JButton addRegister = new JButton("Register");
-                registerPanel.add(addRegister);
+                innerRegisterPanel.add(addRegister);
 
                 addRegister.addActionListener(e -> {
                     try {
@@ -590,8 +539,6 @@ public class GUI {
                         throw new RuntimeException(ex);
                     }
                 });
-
-
                 mainPanel.add(registerPanel, "registerPanel");
             }
         }
